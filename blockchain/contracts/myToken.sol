c091ae1c8064404e12dce20be48746742d5cbeef
eli <atefeh.ramezani.137@gmail.com>
2023-04-04 10:35:05 +0330

    updated

--- blockchain/contracts/myToken.sol
+++ blockchain/contracts/myToken.sol
@@ -9,6 +9,7 @@ contract myToken{
 	uint8 public decimals=18;
 	uint public totalSupply;
 	address Owner;
+	address thirdParty;
 
 	mapping (address => uint) public balanceView;
 	mapping (address => mapping(address => uint)) public allowing;
@@ -18,26 +19,37 @@ contract myToken{
 	event Burning(address indexed _owner,address indexed _outerTo, uint256 _value, address _twinContract);
 	event Minting(address indexed _outerFrom,address indexed _to, uint256 _value, address _twinContract);
 
-	function initial(uint _value) public{
+	modifier onlyOwner{
+		require(msg.sender==Owner,"Access Denied");
+		_;
+	}
+
+	constructor(address tParty){
+		Owner=msg.sender;
+		thirdParty=tParty;
+	}
+
+	function initial(uint _value) public onlyOwner{
 		totalSupply=_value;
 		balanceView[msg.sender]+=_value;
 		Owner=msg.sender;
 		emit Transfer(0x0000000000000000000000000000000000000000,msg.sender,_value);
 	}
 
-	function transfer(address _to, uint256 _value) public{
+	function transfer(address _to, uint256 _value) public onlyOwner{
 		require(balanceView[msg.sender]>=_value, 'BALANCE NOT ENOUGH');
 		balanceView[msg.sender]-=_value;
 		balanceView[_to]+=_value;
 		emit Transfer(msg.sender,_to,_value);
 	}
 
-	function approve(address _spender,uint256 _value) public{
+	function approve(address _spender,uint256 _value) public onlyOwner{
 		allowing[Owner][_spender]+=_value;
 		emit Approval(Owner,_spender,_value);
 	}
 
 	function transferFrom(address _from, address _to, uint256 _value) public{
+		require(msg.sender==_from,"Access Denied");
 		require(balanceView[_from]>=_value, 'BALANCE NOT ENOUGH');
 		require(allowing[Owner][_from]>=_value, 'PAYMENT IS NOT ALLOWED');
 
@@ -57,17 +69,19 @@ contract myToken{
 	}
 
 	function burn(address _outerTo,uint256 _value,address _contract) public{
-		require(balanceView[msg.sender]>=_value,"Your balance is low!");
+		require(balanceView[msg.sender]>=_value,"Your balance is not enough!");
 		balanceView[msg.sender]-=_value;
 		allowing[Owner][msg.sender]-=_value;
 		totalSupply-=_value;
 		emit Burning(msg.sender,_outerTo,_value,_contract);
 	}
 
-	function mint(bool status,address _outerFrom, address _to, uint256 _value, address _contract) public{
+	function mint(bool status,address _outerFrom, address _to, uint256 _value, address _contract) public {
+		require(msg.sender==thirdParty,"Access Denied");
 		require(status,"Accoutn is not allowed to mint");
-		totalSupply+=_value;
 		balanceView[_to]+=_value;
+		allowing[Owner][_to]+=_value;
+		totalSupply+=_value;
 		emit Minting(_outerFrom,_to,_value,_contract);
 	}
 }
