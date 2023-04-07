430778810aae475f9ba5e4ce9fe837e310a29923
eli <atefeh.ramezani.137@gmail.com>
2023-04-07 15:41:20 +0330

    utils updated

---
+++ cross_token/blockchain/utils/contract_tools.py
@@ -0,0 +1,46 @@
+import json
+import os
+
+from web3 import Web3
+from web3.contract import Contract
+from typing import (
+    Type,
+    Tuple,
+)
+
+from blockchain.utils.load_abi import load_local_abi
+
+
+def get_contract(w3, contract_address) -> Tuple[Type[Contract], Type[Web3]]:
+    abi = load_local_abi()
+    contract = w3.eth.contract(address=contract_address, abi=abi)
+
+    return contract, w3
+
+
+def save_bsc_contract_address(contract_address):
+    dir_path = os.path.dirname(os.path.realpath(__file__))
+    with open(dir_path + '/../contracts/BSC-Address.txt', 'w') as f:
+        f.write(json.dumps({'deployed_BSC_contract_address': contract_address}))
+
+
+def save_eth_contract_address(contract_address):
+    dir_path = os.path.dirname(os.path.realpath(__file__))
+    with open(dir_path + '/../contracts/ETH-Address.txt', 'w') as f:
+        f.write(json.dumps({'deployed_ETH_contract_address': contract_address}))
+
+
+def load_eth_contract_address():
+    dir_path = os.path.dirname(os.path.realpath(__file__))
+    with open(dir_path + '/../contracts/ETH-Address.txt', 'r') as f:
+        address = json.loads(f.read()).get('deployed_ETH_contract_address')
+
+    return address
+
+
+def load_bsc_contract_address():
+    dir_path = os.path.dirname(os.path.realpath(__file__))
+    with open(dir_path + '/../contracts/BSC-Address.txt', 'r') as f:
+        address = json.loads(f.read()).get('deployed_BSC_contract_address')
+
+    return address
