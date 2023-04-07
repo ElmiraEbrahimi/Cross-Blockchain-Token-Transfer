e51aba583df6d5b3e4fc28ba1ef764d299bb8977
eli <atefeh.ramezani.137@gmail.com>
2023-04-07 15:44:08 +0330

    model updated

--- cross_token/core/models.py
+++ cross_token/core/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+
+class Config(models.Model):
+    class Meta:
+        verbose_name = 'Config'
+        verbose_name_plural = 'Configs'
+
+    eth_url = models.URLField(blank=True, null=True)
+    bsc_url = models.URLField(blank=True, null=True)
+
+    eth_third_party_address = models.CharField(max_length=255, blank=True, null=True)
+    bsc_third_party_address = models.CharField(max_length=255, blank=True, null=True)
+
+    alice_eth_address = models.CharField(max_length=255, blank=True, null=True)
+    bob_bsc_address = models.CharField(max_length=255, blank=True, null=True)
+
+    deployed_eth_contract_address = models.CharField(max_length=255, blank=True, null=True)
+    deployed_bsc_contract_address = models.CharField(max_length=255, blank=True, null=True)
+    is_contract_compiled = models.BooleanField(default=False)
+    is_eth_deployed = models.BooleanField(default=False)
+    is_bsc_deployed = models.BooleanField(default=False)
+
+    def __str__(self):
+        return 'Config'
