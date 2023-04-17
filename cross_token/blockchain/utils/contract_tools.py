d524073525443a1dc71b3143f8eb6da17dee1f66
eli <atefeh.ramezani.137@gmail.com>
2023-04-17 21:45:43 +0330

    utils updated

--- cross_token/blockchain/utils/contract_tools.py
+++ cross_token/blockchain/utils/contract_tools.py
@@ -9,6 +9,7 @@ from typing import (
 )
 
 from blockchain.utils.load_abi import load_local_abi
+from core.models import Config
 
 
 def get_contract(w3, contract_address) -> Tuple[Type[Contract], Type[Web3]]:
@@ -19,28 +20,40 @@ def get_contract(w3, contract_address) -> Tuple[Type[Contract], Type[Web3]]:
 
 
 def save_bsc_contract_address(contract_address):
-    dir_path = os.path.dirname(os.path.realpath(__file__))
-    with open(dir_path + '/../contracts/BSC-Address.txt', 'w') as f:
-        f.write(json.dumps({'deployed_BSC_contract_address': contract_address}))
+    cf = Config.objects.get()
+    cf.deployed_bsc_contract_address = contract_address
+    cf.is_bsc_deployed = True
+    cf.save()
+    # dir_path = os.path.dirname(os.path.realpath(__file__))
+    # with open(dir_path + '/../contracts/BSC-Address.txt', 'w') as f:
+    #     f.write(json.dumps({'deployed_BSC_contract_address': contract_address}))
 
 
 def save_eth_contract_address(contract_address):
-    dir_path = os.path.dirname(os.path.realpath(__file__))
-    with open(dir_path + '/../contracts/ETH-Address.txt', 'w') as f:
-        f.write(json.dumps({'deployed_ETH_contract_address': contract_address}))
+    cf = Config.objects.get()
+    cf.deployed_eth_contract_address = contract_address
+    cf.is_eth_deployed = True
+    cf.save()
+    # dir_path = os.path.dirname(os.path.realpath(__file__))
+    # with open(dir_path + '/../contracts/ETH-Address.txt', 'w') as f:
+    #     f.write(json.dumps({'deployed_ETH_contract_address': contract_address}))
 
 
 def load_eth_contract_address():
-    dir_path = os.path.dirname(os.path.realpath(__file__))
-    with open(dir_path + '/../contracts/ETH-Address.txt', 'r') as f:
-        address = json.loads(f.read()).get('deployed_ETH_contract_address')
+    cf = Config.objects.get()
+    address = cf.deployed_eth_contract_address
+    # dir_path = os.path.dirname(os.path.realpath(__file__))
+    # with open(dir_path + '/../contracts/ETH-Address.txt', 'r') as f:
+    #     address = json.loads(f.read()).get('deployed_ETH_contract_address')
 
     return address
 
 
 def load_bsc_contract_address():
-    dir_path = os.path.dirname(os.path.realpath(__file__))
-    with open(dir_path + '/../contracts/BSC-Address.txt', 'r') as f:
-        address = json.loads(f.read()).get('deployed_BSC_contract_address')
+    cf = Config.objects.get()
+    address = cf.deployed_bsc_contract_address
+    # dir_path = os.path.dirname(os.path.realpath(__file__))
+    # with open(dir_path + '/../contracts/BSC-Address.txt', 'r') as f:
+    #     address = json.loads(f.read()).get('deployed_BSC_contract_address')
 
     return address
